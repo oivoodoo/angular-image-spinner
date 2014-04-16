5a9b2d674ad759a8394199e0fb4fd4e119cfb876
Alexandr Korsak <alex.korsak@gmail.com>
2014-04-16 21:43:05 +0300

    Build new version

--- dist/angular-image-spinner.js
+++ dist/angular-image-spinner.js
@@ -8,47 +8,50 @@
       return fn.apply(me, arguments);
     };
   };
-  angular.module('imageSpinner').directive('imageSpinner', [
+  angular.module('imageSpinner').constant('imageSpinnerDefaultSettings', {
+    lines: 9,
+    length: 10,
+    width: 2,
+    radius: 3,
+    corners: 1,
+    rotate: 0,
+    direction: 1,
+    color: '#000',
+    speed: 1,
+    trail: 10,
+    shadow: false,
+    hwaccel: false,
+    className: 'spinner',
+    zIndex: 2000000000,
+    top: '40%',
+    left: '30%'
+  }).directive('imageSpinner', [
     '$window',
-    function ($window) {
+    'imageSpinnerDefaultSettings',
+    function ($window, DefaultSettings) {
       var ImageLoader, SPINNER_CLASS_NAME, SpinnerBuilder, link;
       ImageLoader = $window.Image;
       SpinnerBuilder = function () {
-        SpinnerBuilder.prototype.DEFAULT_SETTINGS = {
-          lines: 7,
-          length: 10,
-          width: 2,
-          radius: 3,
-          corners: 1,
-          rotate: 0,
-          direction: 1,
-          color: '#000',
-          speed: 1,
-          trail: 10,
-          shadow: false,
-          hwaccel: false,
-          className: 'spinner',
-          zIndex: 2000000000,
-          top: '40%',
-          left: '30%'
-        };
         function SpinnerBuilder(el, settings) {
-          var height, width;
           this.el = el;
           this.settings = settings;
           this.hide = __bind(this.hide, this);
           if (settings == null) {
             settings = {};
           }
-          settings = angular.extend(settings, this.DEFAULT_SETTINGS);
+          settings = angular.extend(settings, DefaultSettings);
+          this.spinner = new Spinner(settings);
+          this.container = this.el.parent();
+          this.setContainerSize();
+        }
+        SpinnerBuilder.prototype.setContainerSize = function () {
+          var height, width;
           width = this.el.attr('width') || '100';
           height = this.el.attr('height') || '100';
           width = '' + width.replace(/px/, '') + 'px';
           height = '' + height.replace(/px/, '') + 'px';
-          this.container = this.el.parent();
-          angular.element(this.container).css('width', width).css('height', height).css('position', 'relative');
-          this.spinner = new Spinner(settings);
-        }
+          return angular.element(this.container).css('width', width).css('height', height).css('position', 'relative');
+        };
         SpinnerBuilder.prototype.show = function () {
           var loader;
           this.el.css('display', 'none');
